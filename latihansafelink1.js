39110a06710d6c4cc24526bfcfc578d918370224
jinpooci <44929878+jinpooci@users.noreply.github.com>
2018-12-04 20:32:18 +0700

    Update latihansafelink1.js

--- latihansafelink1.js
+++ latihansafelink1.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://tehsarung.blogspot.com/p/blog-page.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://tehsarung.blogspot.com/2018/11/mark-zuckerberg-menghilang-tanpa.html?url=", "https://tehsarung.blogspot.com/2018/11/sel-sel-yang-tersumbat-lemak.html", "https://tehsarung.blogspot.com/2018/11/bagaimana-finlandia-bertahan-tanpa.html" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
